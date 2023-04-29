b5dc9219722cf3a26be1be485df24bdbac6abc3d
S-Andol <sergio.andol@gmail.com>
2023-04-29 10:17:58 -0300

    probando la pag

---
+++ inicio/forms.py
@@ -0,0 +1,11 @@
+# Creamos los formularios
+from django import forms
+
+class CreacionAnimalFormulario(forms.Form):
+    nombre = forms.CharField (max_length=20) 
+    edad =  forms.IntegerField()
+
+
+class BuscarAnimal(forms.Form):
+    nombre = forms.CharField (max_length=20, required=False) 
+    # edad =  forms.IntegerField()
